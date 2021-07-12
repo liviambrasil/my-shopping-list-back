9df6309318656bfa19c56c32078c77d787da67ec
Lívia Brasil <liviabrasil@id.uff.br>
2021-07-12 15:09:56 -0300

    routes post and get items

--- src/app.js
+++ src/app.js
@@ -7,4 +7,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/items', async (req,res) => {
+    const {text} = req.body
+
+    if(!text) return res.sendStatus(401) 
+    try {
+        await connection.query('INSERT INTO list (text) VALUES ($1)', [text])
+        return res.sendStatus(201)
+    }
+    catch (e) {
+        console.log(e)
+        return res.sendStatus(501)
+    }
+})
+
+app.get('/items', async (req,res) => {
+    //res.send(lista de compras)
+    try {
+        const result = await connection.query('SELECT * FROM list')
+        return res.send(result.rows)
+    }
+    catch (e) {
+        console.log(e)
+        return res.sendStatus(501)
+    }
+})
+
 export default app;
